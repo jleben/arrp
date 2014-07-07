c8a56adba5aae208646e7c977a2deca6b0bfb028
Jakob Leben <jakob.leben@gmail.com>
2014-07-06 18:49:27 -0700

    frontend tester: improve command-line arguments

--- frontend/test.cpp
+++ frontend/test.cpp
@@ -7,77 +7,164 @@
 using namespace std;
 using namespace stream;
 
-sp<semantic::type> parse_stream_arg(const string & arg)
+struct evaluation
 {
-    if (arg.size() < 3)
-        throw std::runtime_error("Misformatted stream argument.");
+    string name;
+    vector<sp<semantic::type>> args;
+};
 
-    if (arg.front() != '[' || arg.back() != ']')
-        throw std::runtime_error("Misformatted stream argument.");
+struct argument_parser
+{
+    bool print_tokens = false;
+    bool print_ast = false;
+    vector<evaluation> evaluations;
 
-    string size_list = arg.substr(1, arg.size()-2);
+    argument_parser(int argc, char *argv[]):
+        m_arg_count(argc),
+        m_args(argv)
+    {}
 
-    vector<int> sizes;
-    istringstream size_stream(size_list);
-    string elem;
-    while(std::getline(size_stream, elem, ','))
+    void parse()
+    {
+        while(m_arg_count)
+            parse_next_arg();
+    }
+
+private:
+
+    void advance()
+    {
+        --m_arg_count;
+        ++m_args;
+    }
+
+    void parse_next_arg()
+    {
+        string arg( m_args[0] );
+
+        if (arg == "--print-tokens" || arg == "-t")
+        {
+            print_tokens = true;
+            advance();
+        }
+        else if (arg == "--print-ast" || arg == "-s")
+        {
+            print_ast = true;
+            advance();
+        }
+        else if (arg == "--evaluate" || arg == "--eval" || arg == "-e")
+        {
+            advance();
+            parse_evaluation(arg);
+        }
+        else
+        {
+            ostringstream msg;
+            msg << "Invalid argument: " << arg;
+            throw std::runtime_error(msg.str());
+        }
+    }
+
+    void parse_evaluation(const string & parameter)
+    {
+        evaluations.emplace_back();
+        evaluation &eval = evaluations.back();
+
+        string arg( m_args[0] );
+        if (arg[0] == '-')
+        {
+            ostringstream msg;
+            msg << "Missing function name after " << parameter << " parameter.";
+            throw std::runtime_error(msg.str());
+        }
+
+        eval.name = arg;
+
+        advance();
+
+        while(m_arg_count && m_args[0][0] != '-')
+        {
+            string arg(m_args[0]);
+            eval.args.emplace_back( parse_eval_arg(arg) );
+            advance();
+        }
+    }
+
+    sp<semantic::type> parse_eval_arg(const string & arg)
+    {
+        assert(!arg.empty());
+
+        switch(arg[0])
+        {
+        case '[':
+            return parse_eval_stream_arg(arg);
+        //case '[':
+            //return parse_range_arg(arg);
+        default:
+            return parse_eval_scalar_arg(arg);
+        }
+    }
+
+    sp<semantic::type> parse_eval_stream_arg(const string & arg)
+    {
+        if (arg.size() < 3)
+            throw std::runtime_error("Misformatted stream argument.");
+
+        if (arg.front() != '[' || arg.back() != ']')
+            throw std::runtime_error("Misformatted stream argument.");
+
+        string size_list = arg.substr(1, arg.size()-2);
+
+        vector<int> sizes;
+        istringstream size_stream(size_list);
+        string elem;
+        while(std::getline(size_stream, elem, ','))
+        {
+            try {
+                size_t pos = 0;
+                int size = stoi(elem, &pos);
+                if (pos == elem.size())
+                {
+                    sizes.push_back(size);
+                    continue;
+                }
+            } catch (...) {}
+
+            throw std::runtime_error("Misformatted stream argument.");
+        }
+
+        return sp<semantic::type>( new semantic::stream(sizes) );
+    }
+
+    sp<semantic::type> parse_eval_scalar_arg(const string & arg)
     {
         try {
             size_t pos = 0;
-            int size = stoi(elem, &pos);
-            if (pos == elem.size())
-            {
-                sizes.push_back(size);
-                continue;
-            }
-        } catch (...) {}
+            int i = stoi(arg, &pos);
+            if (pos == arg.size())
+                return sp<semantic::type>( new semantic::integer_num(i) );
+        }
+        catch (...) {}
 
-        throw std::runtime_error("Misformatted stream argument.");
+        try {
+            size_t pos = 0;
+            double d = stod(arg, &pos);
+            if (pos == arg.size())
+                return sp<semantic::type>( new semantic::real_num(d) );
+        }
+        catch (...) {}
+
+        throw std::runtime_error("Misformatted scalar argument.");
     }
 
-    return sp<semantic::type>( new semantic::stream(sizes) );
-}
-
-sp<semantic::type> parse_scalar_arg(const string & arg)
-{
-    try {
-        size_t pos = 0;
-        int i = stoi(arg, &pos);
-        if (pos == arg.size())
-            return sp<semantic::type>( new semantic::integer_num(i) );
-    }
-    catch (...) {}
-
-    try {
-        size_t pos = 0;
-        double d = stod(arg, &pos);
-        if (pos == arg.size())
-            return sp<semantic::type>( new semantic::real_num(d) );
-    }
-    catch (...) {}
-
-    throw std::runtime_error("Misformatted scalar argument.");
-}
-
-sp<semantic::type> parse_range_arg(const string & arg)
-{
-    throw std::runtime_error("Unsupported range argument.");
-}
-
-sp<semantic::type> parse_arg(const string & arg)
-{
-    assert(!arg.empty());
-
-    switch(arg[0])
+    sp<semantic::type> parse_eval_range_arg(const string & arg)
     {
-    case '[':
-        return parse_stream_arg(arg);
-    //case '[':
-        //return parse_range_arg(arg);
-    default:
-        return parse_scalar_arg(arg);
+        throw std::runtime_error("Unsupported range argument.");
     }
-}
+
+    int m_arg_count;
+    char **m_args;
+};
 
 int main(int argc, char *argv[])
 {
@@ -91,81 +178,75 @@ int main(int argc, char *argv[])
     }
     input_filename = argv[1];
 
-    if (argc > 2)
-    {
-        function_name = argv[2];
+    argument_parser args(argc-2, argv+2);
+
+    try {
+        args.parse();
     }
-    else
+    catch (exception & e)
     {
-        function_name = "main";
-        cerr << "WARNING: No starting function name given." << endl
-             << "WARNING: Assuming default: " << function_name << endl;
+        cerr << "ERROR: Command line: " << e.what() << endl;
+        return 0;
     }
 
-    vector<sp<semantic::type>> function_args;
-    for (int i = 3; i < argc; ++i)
+    ifstream input_file(input_filename);
+
+    stream::Parser parser(input_file);
+    parser.setPrintsTokens(args.print_tokens);
+
+    if (args.print_tokens)
+        cout << "== Tokens ==" << endl;
+
+    int success = parser.parse();
+    if (success != 0)
+        return success;
+
+    if (args.print_ast)
     {
-        try {
-            function_args.emplace_back( parse_arg(argv[i]) );
-        } catch (exception & e)
+        cout << endl;
+        cout << "== Abstract Syntax Tree ==" << endl;
+        stream::ast::printer printer;
+        printer.print( parser.ast().get() );
+        cout << endl;
+    }
+
+    if (!args.evaluations.empty())
+    {
+        cout << endl;
+        cout << "== Semantic Analysis ==" << endl;
+        stream::semantic::environment env = stream::semantic::top_environment( parser.ast().get() );
+
+        for (const evaluation & eval : args.evaluations)
         {
-            cerr << "ERROR while parsing arguments: " << e.what() << endl;
-            return 0;
+            cout << "-- Evaluating: " << eval.name;
+            cout << "(";
+            if (eval.args.size())
+                cout << *eval.args.front();
+            for ( int i = 1; i < eval.args.size(); ++i )
+            {
+                cout << ", ";
+                cout << *eval.args[i];
+            }
+            cout << ")" << endl;
+
+            stream::semantic::symbol *sym = env[eval.name];
+            if (!sym)
+            {
+                cerr << "WARNING: no symbol '" << eval.name << "' available." << endl;
+                continue;
+            }
+
+            try {
+                sp<semantic::type> value = sym->evaluate(env, eval.args);
+                if (value)
+                    cout << "Result type = " << *value << endl;
+                else
+                    cout << "Result type = void" << endl;
+            }
+            catch (semantic::semantic_error & e)
+            {
+                e.report();
+            }
         }
     }
-
-  ifstream input_file(input_filename);
-
-  stream::Parser parser(input_file);
-
-  int success = parser.parse();
-
-  if (success != 0)
-      return success;
-
-  cout << endl;
-
-  cout << "== Abstract Syntax Tree ==" << endl;
-  stream::ast::printer printer;
-  printer.print( parser.ast().get() );
-  cout << endl;
-
-  cout << endl;
-
-  cout << "== Semantic Analysis ==" << endl;
-  stream::semantic::environment env = stream::semantic::top_environment( parser.ast().get() );
-
-  stream::semantic::symbol *main = env[function_name];
-  if (!main)
-  {
-      cerr << "WARNING: no function named '" << function_name << "' available." << endl;
-      return 0;
-  }
-
-  //vector<std::shared_ptr<semantic::type>> args;
-  //args.emplace_back( new semantic::stream({10,20,30}) );
-  //args.emplace_back( new semantic::stream({10,20,30}) );
-
-  cout << "Evaluating: " << function_name;
-  cout << "(";
-  if (function_args.size())
-      cout << *function_args.front();
-  for ( int i = 1; i < function_args.size(); ++i )
-  {
-      cout << ", ";
-      cout << *function_args[i];
-  }
-  cout << ")" << endl;
-
-  try {
-      sp<semantic::type> value = main->evaluate(env, function_args);
-      if (value)
-          cout << "Result type = " << *value << endl;
-      else
-          cout << "Result type = void" << endl;
-  }
-  catch (semantic::semantic_error & e)
-  {
-      e.report();
-  }
 }
