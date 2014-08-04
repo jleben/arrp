707cffe5842a4857f970eb496ada2ca6e8a224b7
Jakob Leben <jakob.leben@gmail.com>
2014-08-04 11:01:28 -0700

    test: use multi-dimensional stream arithmetic

--- test/test.c
+++ test/test.c
@@ -7,18 +7,19 @@ double* result(double*);
 
 int main()
 {
-    double in[10];
+    const unsigned int count = 25;
+
+    double in[count];
     int i;
 
-    for (i = 0; i < 10; ++i)
+    for (i = 0; i < count; ++i)
         in[i] = i;
 
     double *out = result(in);
 
     printf("out: ");
-    for (i = 0; i < 10; ++i)
-        printf("%f,  ", out[i]);
-    printf("\n");
+    for (i = 0; i < count; ++i)
+        printf("%f\n", out[i]);
 
     return 0;
 }
