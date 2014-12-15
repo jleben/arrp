39ea6c59e0e148553ec517fb6f6549ca2d30ac39
Jakob Leben <jakob.leben@gmail.com>
2014-12-15 11:09:55 -0800

    CMake: fix building integration tests

--- test/integration/mat-mul.cpp
+++ test/integration/mat-mul.cpp
@@ -18,7 +18,7 @@ along with this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
-#include "matrix-mult.h"
+#include "mat-mul.h"
 #include "test.hpp"
 
 #include <iostream>
