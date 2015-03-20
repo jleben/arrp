a9845a4582e2e9558f4380b821da6724cc6d2eed
Jakob Leben <jakob.leben@gmail.com>
2015-03-20 13:44:32 -0600

    CMake: modularize stream kernel compilation steps

--- cmake/streams.cmake
+++ cmake/streams.cmake
@@ -1,3 +1,23 @@
+function(stream_kernel_opt out in)
+  add_custom_command(OUTPUT ${out}
+    COMMAND opt
+    ARGS -S -scalarrepl -std-compile-opts ${in} -o ${out}
+    DEPENDS ${in}
+  )
+endfunction()
+
+function(stream_kernel_object out in)
+  if(CMAKE_CL_64)
+    set(arch -march=x86-64)
+  endif()
+
+  add_custom_command(OUTPUT ${out}
+    COMMAND ${llc_program}
+    ARGS ${arch} -filetype=obj -o ${out} ${in} $<$<CONFIG:Release>:-O=3>
+    DEPENDS ${in}
+  )
+endfunction()
+
 function(compile_stream_func target_name source_file sym_name args options)
 
   set(llvm_file ${target_name}.ll)
