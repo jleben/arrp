edede68eeb9bdc5b1683db9b116cea699f973e13
Jakob Leben <jakob.leben@gmail.com>
2016-05-10 00:07:21 -0700

    test: fm_radio: adapt for new C++ interface, add callgrind instrumentation

--- test/performance/fm_radio/fm_radio_driver.cpp
+++ test/performance/fm_radio/fm_radio_driver.cpp
@@ -1,16 +1,16 @@
-#include "fm_radio.cpp"
+#include "fm_radio_s_kernel.cpp"
 #include "../drivers/test_driver.hpp"
-
+#include <valgrind/callgrind.h>
 #include <iostream>
 #include <iomanip>
 
-#define PRINT 0
+#define PRINT 1
 
 using namespace std;
 
-namespace fm_radio {
+namespace fm_radio_s_kernel {
 static volatile float v;
-inline void output(float * data)
+inline void state::output(float * data)
 {
 #if PRINT
     cout << std::showpoint << std::setprecision(15) << (double) *data << endl;
@@ -22,22 +22,46 @@ inline void output(float * data)
 
 struct fm_radio_test
 {
-    fm_radio::state s;
+    fm_radio_s_kernel::state s;
     void initialize()
     {
-        fm_radio::initialize(&s);
+        s = fm_radio_s_kernel::state();
+        s.initialize();
     }
     void run()
     {
-        for (int i = 0; i < 100; ++i)
-            fm_radio::process(&s);
+        CALLGRIND_TOGGLE_COLLECT;
+        for (int i = 0; i < 500; ++i)
+            s.process();
+        CALLGRIND_TOGGLE_COLLECT;
     }
 };
 
 int main()
 {
+#if PRINT
+
+    fm_radio_s_kernel::state s;
+
+    s.initialize();
+
+    for (int i = 0; i < 20; ++i)
+        s.process();
+
+#else // PRINT
+
     fm_radio_test test;
 
+#if 1
+    for (int i = 0; i < 1000; ++i)
+    {
+        test.initialize();
+        test.run();
+    }
+#else
     test_driver<fm_radio_test> driver;
     driver.go(test, 3, 1000);
+#endif
+
+#endif // PRINT
 }
