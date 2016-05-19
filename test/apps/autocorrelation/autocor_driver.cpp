06ce8166f1fb1713c439b76c02115d223c9961f4
Jakob Leben <jakob.leben@gmail.com>
2016-05-19 16:56:24 -0700

    test/apps: add autocorrelation

---
+++ test/apps/autocorrelation/autocor_driver.cpp
@@ -0,0 +1,30 @@
+#include "autocor_kernel.cpp"
+
+#include <iostream>
+
+using namespace std;
+
+static const int out_size = 19;
+
+class autocor_printer : public autocorrelation::state<autocor_printer>
+{
+public:
+    void output(float * data)
+    {
+        for (int i = 0; i < out_size; ++i)
+        {
+            cout << i << ":" << data[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+int main()
+{
+    auto printer = new autocor_printer;
+    printer->initialize();
+    for (int i = 0; i < 10; ++i)
+    {
+        printer->process();
+    }
+}
