0c6347d805dcbcfeccd119c321d537ec1b9d3085
Jakob Leben <jakob.leben@gmail.com>
2016-08-21 10:41:33 -0700

    add isl as submodule

--- cmake/Findisl.cmake
+++ cmake/Findisl.cmake
@@ -1,6 +1,6 @@
 include(FindPackageHandleStandardArgs)
 
-find_path(ISL_INCLUDE_DIR isl/ctx.h HINTS extra/isl/include)
-find_library(ISL_LIBRARY isl HINTS extra/isl/lib)
+find_path(ISL_INCLUDE_DIR isl/ctx.h HINTS extra/isl/build/include)
+find_library(ISL_LIBRARY isl HINTS extra/isl/build/lib)
 
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(isl DEFAULT_MSG ISL_INCLUDE_DIR ISL_LIBRARY)
