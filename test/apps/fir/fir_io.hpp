720d575c71f836b1a24d72f2d971091faba2d16a
Jakob Leben <jakob.leben@gmail.com>
2016-09-30 19:30:32 -0700

    test/apps/fir: switch to auto-testing

---
+++ test/apps/fir/fir_io.hpp
@@ -0,0 +1,28 @@
+#pragma once
+#include <io.hpp>
+
+class program_id;
+
+namespace arrp {
+namespace testing {
+
+template<>
+class io<program_id> : public io_base
+{
+public:
+    void input_coefs(double k[3])
+    {
+        for (int i = 0; i < 3; ++i)
+            k[i] = i+1;
+    }
+
+    void input_in1(double &v)
+    {
+        static int t = 0;
+        v = t;
+        t += 1;
+    }
+};
+
+}
+}
