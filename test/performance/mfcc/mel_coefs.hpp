b84684b5138d5c774c5f8ab7a53c62325dda4e68
Jakob Leben <jakob.leben@gmail.com>
2016-11-01 16:19:49 -0700

    test/perf: add mfcc

---
+++ test/performance/mfcc/mel_coefs.hpp
@@ -0,0 +1,60 @@
+#pragma once
+
+#include <cmath>
+#include <vector>
+
+namespace mfcc {
+
+using std::vector;
+
+double freq_to_mel(double freq)
+{
+    return 2595.0 * std::log10(1.0 + freq/700.0);
+}
+
+double mel_to_freq(double mel)
+{
+    return 700.0 * (std::pow(10.0, mel/2595.0) - 1.0);
+}
+
+int freq_to_bin(double sr, int n, double f) { return n*f/sr; }
+
+double bin_to_freq(double sr, int n, int b) { return b*sr/n; }
+
+double coef(double l, double c, double h, double x)
+{
+    if (x < l || x > h)
+        return 0;
+    else if (x <= c)
+        return (x-l)/(c-l);
+    else
+        return (h-x)/(h-c);
+}
+
+vector<double> mel_freqs(double lf, double hf, int n)
+{
+    double lm = freq_to_mel(lf);
+    double hm = freq_to_mel(hf);
+    vector<double> m;
+    for (int i = 0; i < n; ++i)
+    {
+        m.push_back(mel_to_freq(i/float(n-1)) * (hm - lm) + lm);
+    }
+    return m;
+}
+
+template <int N, int WN>
+void mel_coefs(double sr, double fl, double fh, double coefs[N][WN/2+1])
+{
+    auto freq_grid = mel_freqs(fl, fh, N+2);
+    for (int n = 0; n < N; ++n)
+    {
+        for (int k = 0; k < WN/2+1; ++k)
+        {
+            coefs[n][k] = coef
+                    (freq_grid[n], freq_grid[n+1], freq_grid[n+2], bin_to_freq(sr,WN,k));
+        }
+    }
+}
+
+}
---
+++ test/performance/mfcc/mfcc_io.hpp
@@ -0,0 +1,69 @@
+ 
+#pragma once
+#include "mel_coefs.hpp"
+#include <io.hpp>
+#include <complex>
+#include <fftw3.h>
+
+class program_id;
+
+namespace arrp {
+namespace testing {
+
+template<>
+class io<test::traits>
+{
+public:
+    static const int win_size = 64;
+    static const int fft_size = win_size/2 + 1;
+    static const int mfcc_size = 10;
+
+    fftw_plan m_dft_plan;
+    fftw_plan m_dct_plan;
+
+    double sample_rate;
+    double freq_lower_bound;
+    double freq_upper_bound;
+
+    io(): io(20000, 100, 10000)
+    {
+
+    }
+
+    io(double sr, double fl, double fh):
+        sample_rate(sr),
+        freq_lower_bound(fl),
+        freq_upper_bound(fh)
+    {
+        {
+            auto in = fftw_alloc_real(win_size);
+            auto out = fftw_alloc_complex(fft_size);
+            m_dft_plan = fftw_plan_dft_r2c_1d(win_size, in, out, 0);
+            fftw_free(in);
+            fftw_free(out);
+        }
+        {
+            auto in = fftw_alloc_real(mfcc_size);
+            auto out = fftw_alloc_real(mfcc_size);
+            m_dct_plan = fftw_plan_r2r_1d(mfcc_size, in, out, FFTW_REDFT10, 0);
+        }
+    }
+
+    void fft(double in[win_size], complex<double> out[fft_size])
+    {
+        fftw_execute_dft_r2c(m_dft_plan, in, reinterpret_cast<fftw_complex*>(out));
+    }
+
+    void dct(double in[mfcc_size], double out[mfcc_size])
+    {
+        fftw_execute_r2r(m_dct_plan, in, out);
+    }
+
+    void input_mel_coefs(double out[mfcc_size][fft_size])
+    {
+        mfcc::mel_coefs<mfcc_size, win_size>(sample_rate, freq_lower_bound, freq_upper_bound, out);
+    }
+};
+
+}
+}
