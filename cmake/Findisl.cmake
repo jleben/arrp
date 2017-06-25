6ec04aa19cb9ae4169e3e3b91053b792c235f166
Jakob Leben <jakob.leben@gmail.com>
2017-06-25 11:58:25 -0700

    CMake: Use static isl and arrp-lib libraries to minimize dependencies

--- cmake/Findisl.cmake
+++ cmake/Findisl.cmake
@@ -1,6 +1,18 @@
 include(FindPackageHandleStandardArgs)
 
-find_path(ISL_INCLUDE_DIR isl/ctx.h HINTS extra/isl/build/include)
-find_library(ISL_LIBRARY isl HINTS extra/isl/build/lib)
+if (NOT ISL_INCLUDE_DIR OR NOT ISL_LIBRARY)
+    find_path(ISL_INCLUDE_DIR isl/ctx.h PATHS extra/isl/build/include NO_DEFAULT_PATH)
+    find_library(ISL_LIBRARY libisl.a PATHS extra/isl/build/lib NO_DEFAULT_PATH)
+endif()
+
+if (NOT ISL_INCLUDE_DIR AND NOT ISL_LIBRARY)
+    find_path(ISL_INCLUDE_DIR isl/ctx.h)
+    find_library(ISL_LIBRARY isl)
+endif()
+
+string(FIND "${ISL_LIBRARY}" "${CMAKE_SOURCE_DIR}" SOURCE_DIR_POS)
+if (SOURCE_DIR_POS EQUAL 0)
+    set(ISL_LIBRARY_IS_INTERNAL TRUE CACHE BOOL "")
+endif()
 
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(isl DEFAULT_MSG ISL_INCLUDE_DIR ISL_LIBRARY)
