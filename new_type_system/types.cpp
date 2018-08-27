7df54bcda281022d48da15a0da035b50a075afbd
Jakob Leben <jakob.leben@gmail.com>
2018-08-27 13:56:48 -0700

    Basic type system structures and algorithms

---
+++ new_type_system/types.cpp
@@ -0,0 +1,249 @@
+#include "types.hpp"
+#include "../common/error.hpp"
+#include "../utility/printing.hpp"
+
+#include <ostream>
+
+using namespace std;
+
+namespace arrp {
+
+static type_ptr unify_collapsed(const type_ptr & a, const type_ptr & b);
+
+type_ptr unify(const type_ptr & a_raw, const type_ptr & b_raw)
+{
+    type_ptr a = collapse(a_raw);
+    type_ptr b = collapse(b_raw);
+    return unify_collapsed(a,b);
+}
+
+type_ptr unify_collapsed(const type_ptr & a, const type_ptr & b)
+{
+    if (auto a_var = dynamic_pointer_cast<type_var>(a))
+    {
+        if (auto b_var = dynamic_pointer_cast<type_var>(b))
+        {
+            auto result_var = shared(new type_var);
+            result_var->constraints.insert(a_var->constraints.begin(), a_var->constraints.end());
+            result_var->constraints.insert(b_var->constraints.begin(), b_var->constraints.end());
+            a_var->constraints.clear();
+            b_var->constraints.clear();
+
+            a_var->value = b_var->value = result_var;
+
+            return result_var;
+        }
+        else if (is_contained(a_var, b))
+        {
+            ostringstream msg;
+            msg << "Variable " << *a << " is contained in type " << *b << ".";
+            throw type_error(msg.str());
+        }
+        else
+        {
+            a_var->constraints.clear();
+            a_var->value = b;
+            return b;
+        }
+    }
+    else if (auto a_cons = dynamic_pointer_cast<type_cons>(a))
+    {
+        if (auto b_var = dynamic_pointer_cast<type_var>(b))
+        {
+            // Already handled above
+            return unify_collapsed(b, a);
+        }
+        else if (auto b_cons = dynamic_pointer_cast<type_cons>(b))
+        {
+            if (a_cons->kind != b_cons->kind ||
+                    a_cons->arguments.size() != b_cons->arguments.size())
+            {
+                ostringstream msg;
+                msg << "Type mismatch: " << *a << " != " << *b;
+                throw type_error(msg.str());
+            }
+
+            for (int i = 0; i < (int)a_cons->arguments.size(); ++i)
+            {
+                auto unified_arg = unify_collapsed(a_cons->arguments[i], b_cons->arguments[i]);
+                a_cons->arguments[i] = unified_arg;
+                b_cons->arguments[i] = unified_arg;
+            }
+
+            return a_cons;
+        }
+    }
+
+    throw stream::error("Unexpected kind of type.");
+}
+
+type_ptr follow(const type_ptr & t)
+{
+    if (auto v = dynamic_pointer_cast<type_var>(t))
+    {
+        if (v->value)
+            return follow(v->value);
+        else
+            return v;
+    }
+
+    return t;
+}
+
+type_ptr collapse(const type_ptr & t)
+{
+    if (auto v = dynamic_pointer_cast<type_var>(t))
+    {
+        if (v->value)
+            return collapse(v->value);
+        else
+            return v;
+    }
+    else if (auto cons = dynamic_pointer_cast<type_cons>(t))
+    {
+        for (auto & arg : cons->arguments)
+        {
+            arg = collapse(arg);
+        }
+        return cons;
+    }
+    else
+    {
+        throw stream::error("Unexpected kind of type.");
+    }
+}
+
+bool is_contained(const type_var_ptr & v, const type_ptr & t)
+{
+    if (auto cons = dynamic_pointer_cast<type_cons>(t))
+    {
+        for (auto & arg : cons->arguments)
+        {
+            if (is_contained(v, arg))
+                return true;
+        }
+        return false;
+    }
+    else
+    {
+        return v == t;
+    }
+}
+
+using type_var_map = unordered_map<type_var_ptr, type_var_ptr>;
+
+static
+type_ptr recursive_copy(type_var_map & vars, type_ptr type)
+{
+    type = follow(type);
+
+    if (auto var = dynamic_pointer_cast<type_var>(type))
+    {
+        {
+            auto instance_pos = vars.find(var);
+            if (instance_pos != vars.end())
+            {
+                cout << "Reusing already copied var " << var << endl;
+                return instance_pos->second;
+            }
+        }
+
+        if (var->is_universal)
+        {
+            cout << "Copying universal var " << var << endl;
+
+            auto instance = shared(new type_var);
+            instance->constraints = var->constraints;
+
+            vars.emplace(var, instance);
+
+            return instance;
+        }
+        else
+        {
+            cout << "Reusing non-universal var " << var << endl;
+            return var;
+        }
+    }
+    else if (auto cons = dynamic_pointer_cast<type_cons>(type))
+    {
+        auto new_cons = shared(new type_cons(cons->kind));
+        for (auto & arg : cons->arguments)
+        {
+            new_cons->arguments.push_back(recursive_copy(vars, arg));
+        }
+        return new_cons;
+    }
+    else
+    {
+        throw stream::error("Unexpected kind of type.");
+    }
+}
+
+type_ptr copy(const type_ptr & type)
+{
+    type_var_map map;
+    return recursive_copy(map, type);
+}
+
+void type_constructor::print(ostream & out, const vector<type_ptr> & arguments)
+{
+    out << name;
+    if (!arguments.empty())
+    {
+        out << "(";
+        out << printable(arguments, ", ");
+        out << ")";
+    }
+}
+
+ostream & operator<<(ostream & out, const type & t)
+{
+    if (auto v = dynamic_cast<const type_var*>(&t))
+    {
+        out << *v;
+    }
+    else if (auto c = dynamic_cast<const type_cons*>(&t))
+    {
+        out << *c;
+    }
+    else
+    {
+        out << "?";
+    }
+
+    return out;
+}
+
+ostream & operator<<(ostream & out, const type_var & v)
+{
+    if (v.value)
+    {
+        out << "<" << &v << "> = " << *v.value;
+    }
+    else
+    {
+        if (!v.constraints.empty())
+        {
+            out << printable(v.constraints, ", ");
+            out << " => ";
+        }
+        out << "<" << &v << ">";
+    }
+
+    return out;
+}
+
+ostream & operator<<(ostream & out, const type_cons & t)
+{
+    t.kind->print(out, t.arguments);
+    return out;
+}
+
+ostream & operator<<(ostream & out, const type_class & c)
+{
+    out << c.name;
+    return out;
+}
+
+}
---
+++ new_type_system/types.hpp
@@ -0,0 +1,121 @@
+#pragma once
+
+#include "../common/error.hpp"
+#include <memory>
+#include <iostream>
+#include <unordered_set>
+#include <vector>
+#include <string>
+#include <functional>
+
+namespace arrp {
+
+using std::shared_ptr;
+using std::ostream;
+using std::unordered_set;
+using std::vector;
+using std::string;
+
+template <typename T>
+inline
+std::shared_ptr<T> shared(T * value)
+{
+    return shared_ptr<T>(value);
+}
+
+class type_error : public stream::error
+{
+public:
+    type_error(const string & msg):
+        error(msg)
+    {}
+};
+
+class type;
+class type_var;
+class type_constructor;
+class type_cons;
+class type_class;
+
+using type_ptr = std::shared_ptr<type>;
+using type_var_ptr = std::shared_ptr<type_var>;
+using type_constructor_ptr = std::shared_ptr<type_constructor>;
+using type_cons_ptr = std::shared_ptr<type_cons>;
+using type_class_ptr = std::shared_ptr<type_class>;
+
+class type
+{
+public:
+    virtual ~type() {}
+};
+
+class type_var : public type
+{
+public:
+    type_var() {}
+
+    unordered_set<type_class_ptr> constraints;
+    type_ptr value;
+    bool is_universal = false;
+};
+
+class type_constructor
+{
+public:
+    string name;
+
+    type_constructor(const string & name):
+        name(name) {}
+
+    virtual ~type_constructor() {}
+    virtual void print(ostream & out, const vector<type_ptr> & arguments);
+};
+
+class type_cons : public type
+{
+public:
+    type_cons(type_constructor_ptr kind, const vector<type_ptr> & args = vector<type_ptr>()):
+        kind(kind), arguments(args) {}
+
+    type_constructor_ptr kind;
+    vector<type_ptr> arguments;
+};
+
+
+class type_class
+{
+public:
+    type_class(const string & name): name(name) {}
+
+    struct instance
+    {
+        vector<type_ptr> args;
+        type_ptr type;
+    };
+
+    string name;
+    using instantiator = std::function<instance()>;
+    vector<instantiator> members;
+};
+
+type_ptr unify(const type_ptr &, const type_ptr &);
+
+type_ptr follow(const type_ptr &);
+type_ptr collapse(const type_ptr &);
+
+type_ptr copy(const type_ptr &);
+
+bool is_contained(const type_var_ptr &, const type_ptr &);
+
+ostream & operator<<(ostream & out, const type &);
+ostream & operator<<(ostream & out, const type_var & v);
+ostream & operator<<(ostream & out, const type_cons & t);
+ostream & operator<<(ostream & out, const type_class & c);
+
+inline ostream & operator<<(ostream & out, const type_ptr & t)
+{
+    out << *t;
+    return out;
+}
+
+}
